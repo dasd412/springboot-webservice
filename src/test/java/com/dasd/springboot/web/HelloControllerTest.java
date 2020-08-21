9d9ca730ba6810df1df07e727686b410869edcd6
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-08-21 15:47:54 +0900

    chapter_02 test code updated. And added application.properties

---
+++ src/test/java/com/dasd/springboot/web/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.dasd.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello_test()throws Exception{
+        String hello="hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+}
