85b77b6e1f3ed0e386f2bf797cb3378173738f40
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-09-05 13:40:53 +0900

    OAuth2 in chapter 05 updated.

---
+++ src/main/java/com/dasd/springboot/configure/auth/dto/SessionUser.java
@@ -0,0 +1,21 @@
+package com.dasd.springboot.configure.auth.dto;
+
+import com.dasd.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private  String name;
+    private  String email;
+    private String picture;
+
+
+    public SessionUser(User user) {
+        this.name=user.getName();
+        this.email=user.getEmail();
+        this.picture=user.getPicture();
+    }
+}
--- src/main/java/com/dasd/springboot/web/IndexController.java
+++ src/main/java/com/dasd/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dasd.springboot.web;
 
+import com.dasd.springboot.configure.auth.dto.SessionUser;
 import com.dasd.springboot.service.posts.PostsService;
 import com.dasd.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){//Model 객체는 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장한다.
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user=(SessionUser)httpSession.getAttribute("user");
+
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
 
         return "index";
     }
